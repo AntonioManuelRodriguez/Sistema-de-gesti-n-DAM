da6b748322fa761e7cad1754d40bb1b2960a0b61
Antonio Manuel Rodriguez Palenzuela <kxlx54esp@gmail.com>
2026-09-24 15:06:14 +0200

    Commit inicial. Creacion de clase Categoria

---
+++ src/main/java/org/example/Model/Categoria.java
@@ -0,0 +1,13 @@
+package org.example.Model;
+
+/**
+ *
+ * @author Antonio Manuel Rodriguez Palenzuela
+ * @version 1.0
+ * @since 24/09/2025
+ * Esta clase lo que llevaremos a cabo es la creación de la clase Categoria, que nos servirá para crear las categorias que se van a vender en la tienda.
+ *
+ */
+public class Categoria {
+    
+}
