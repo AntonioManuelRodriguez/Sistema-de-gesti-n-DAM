299a3d9fad0ea9f4bf3e4a6dd1c0ddb9f7ca5c84
Antonio Manuel Rodriguez Palenzuela <kxlx54esp@gmail.com>
2026-09-24 15:35:28 +0200

    Clase App - Configuración y arranque de la interfaz gráfica con JavaFX.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,24 @@
 package org.example;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("/view/principal.fxml"));
+        Parent root = loader.load();
+
+        stage.setTitle("Gestión de inventario");
+        stage.setScene(new Scene(root, 900, 600));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
