abbb634710b63ac721f9309b3044677ea8940ae1
Antonio Manuel Rodriguez Palenzuela <kxlx54esp@gmail.com>
2026-09-24 15:06:59 +0200

    Clase Categoria : Creacion de atributos de constructores y getter y setter

--- src/main/java/org/example/Model/Categoria.java
+++ src/main/java/org/example/Model/Categoria.java
@@ -9,5 +9,31 @@ package org.example.Model;
  *
  */
 public class Categoria {
-    
+    // Atributos de la clase Categoria
+    private int id;
+    private String nombre;
+
+    //Constructor
+    public Categoria(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    // Getters y Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
